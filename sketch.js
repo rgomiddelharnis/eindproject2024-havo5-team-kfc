644a0555f5a568a811b15e842dd94be62e5f9669
Jaron Lugtenburg <jaronlugtenburg@dogg0d.dev>
2024-03-22 09:28:45 +0100

    Update

--- sketch.js
+++ sketch.js
@@ -20,13 +20,19 @@ let level = 1;
 function preload() {
     // Background asset
     assetBackgroundMainMenu = loadImage("asset/background/mainMenu.jpg");
-    assetGameBackGround = loadImage("images/image-3.png")
+    assetGameBackGround = loadImage("asset/background/Speelveld.jpg")
 
     // Button asset
     assetButtonStart = loadImage("asset/button/start.png");
     assetButtonSettings = loadImage("asset/button/settings.png")
 
     // HUD asset
+
+    // Ghost asset
+    assetGhostNormal = loadImage("asset/ghost/normal.png")
+    assetGhostGangster = loadImage("asset/ghost/gangster.png")
+    assetGhostMysterious = loadImage("asset/ghost/mysterious.png")
+    assetGhostFemale = loadImage("asset/ghost/female.png")
 
     // Levels
     // levels = loadJSON('levels.json');
